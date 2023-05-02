a9ab580b329350a9c07225ade02a9b269e95bb8e
Khubab <khubab220@gmail.com>
2023-05-02 10:46:40 +0000

    Add use context function to get username on log in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext)
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = <><NavLink exact
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/signin"
+  >
+    <i className="fas fa-sign-in-alt"></i>Sign In
+  </NavLink>
+    <NavLink exact
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/signup"
+    >
+      <i className="fas fa-user-plus"></i>Sign Up
+    </NavLink></>
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -23,20 +41,7 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-sign-in-alt"></i>Sign In
-            </NavLink>
-            <NavLink exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signup"
-            >
-              <i className="fas fa-user-plus"></i>Sign Up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
