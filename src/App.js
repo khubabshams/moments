89bd0827be3778657f92666ab09e8c625a613f20
kshamse <khubab220@gmail.com>
2023-05-02 06:54:14 +0100

    Add route and switch to all home links and style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={()=><p>Page not found 404!</p>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
