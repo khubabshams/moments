407aff535538d0cede71fc8fcf0e2066056cb989
Khubab <khubab220@gmail.com>
2023-05-02 09:23:17 +0000

    Add errors to signup

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -81,6 +81,11 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm Password</Form.Label>
@@ -93,6 +98,11 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
@@ -100,6 +110,12 @@ const SignUpForm = () => {
             >
               Sign up
             </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" className="mt-3" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
