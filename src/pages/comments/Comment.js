34aee98c86987529e825967c15c9fd526f20f75a
Khubab <khubab220@gmail.com>
2023-05-07 10:57:08 +0000

    Add dropdown to comments of it's owner

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,8 +3,44 @@ import styles from "../../styles/Comment.module.css"
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import Avatar from '../../components/Avatar'
 import { Media } from 'react-bootstrap'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import { MoreDropdown } from '../../components/MoreDropdown'
+import { axiosRes } from '../../api/axiosDefaults'
 
-const Comment = ({ profile_id, profile_image, owner, updated_at, content }) => {
+const Comment = (props) => {
+
+    const {
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`)
+            setPost(prevPost => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1,
+                }]
+            }))
+            setComments(prevComments => ({
+                ...prevComments,
+                results: prevComments.results.filter(comment => comment.id !== id),
+            }))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div>
             <hr />
@@ -12,7 +48,7 @@ const Comment = ({ profile_id, profile_image, owner, updated_at, content }) => {
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} height={55} />
                 </Link>
-                <Media.Body  className="align-self-center ml-2">
+                <Media.Body className="align-self-center ml-2">
                     <span className={styles.Owner}>
                         {owner}
                     </span>
@@ -21,6 +57,11 @@ const Comment = ({ profile_id, profile_image, owner, updated_at, content }) => {
                     </span>
                     <p>{content}</p>
                 </Media.Body>
+                {
+                    is_owner && (
+                        <MoreDropdown handleDelete={handleDelete} handleEdit={() => { }} />
+                    )
+                }
             </Media>
         </div>
     )
