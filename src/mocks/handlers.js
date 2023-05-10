cc503218c915f7f8a98b6559529947f6adb6f2ec
Khubab <khubab220@gmail.com>
2023-05-10 16:35:08 +0000

    Add test setup and handlers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,18 @@
+const baseURL = "https://moments23.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            pk: 4,
+            username: "ahmedo",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 4,
+            profile_image: "https://res.cloudinary.com/duv51g0vq/image/upload/v1/media/../default_profile_llqwrh"
+        }))
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res,ctx)=>{
+        return res(ctx.status(200));
+    })
+];
